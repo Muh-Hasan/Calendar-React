04713e51b19d963988e9f4fc97335be7d426af8d
Muh-Hasan <muh.hasan.git@gmail.com>
2020-09-23 07:58:17 -0700

    created calendar working

--- src/Calender/index.js
+++ src/Calender/index.js
@@ -8,7 +8,89 @@ import { INITIAL_EVENTS, createEventId } from "./utills";
 export default function Calendar(props) {
   const [weekendsVisible, setweekendsVisible] = useState(true);
   const [currentEvents, setcurrentEvents] = useState([]);
+  const renderSidebar = () => {
+    return (
+      <div className="demo-app-sidebar">
+        <div className="demo-app-sidebar-section">
+          <h2>Instructions</h2>
+          <ul>
+            <li>Select dates and you will be prompted to create a new event</li>
+            <li>Drag, drop, and resize events</li>
+            <li>Click an event to delete it</li>
+          </ul>
+        </div>
+        <div className="demo-app-sidebar-section">
+          <label>
+            <input
+              type="checkbox"
+              checked={weekendsVisible}
+              onChange={handleWeekendsToggle}
+            ></input>
+            toggle weekends
+          </label>
+        </div>
+        <div className="demo-app-sidebar-section">
+          <h2>All Events ({currentEvents.length})</h2>
+          <ul>{currentEvents.map(renderSidebarEvent)}</ul>
+        </div>
+      </div>
+    );
+  };
   
+  const handleWeekendsToggle = () => {
+    setweekendsVisible(!weekendsVisible);
+  };
+  const handleDateSelect = (selectInfo) => {
+    let title = prompt("Please enter a new title for your event");
+    let calendarApi = selectInfo.view.calendar;
+  
+    calendarApi.unselect(); // clear date selection
+  
+    if (title) {
+      calendarApi.addEvent({
+        id: createEventId(),
+        title,
+        start: selectInfo.startStr,
+        end: selectInfo.endStr,
+        allDay: selectInfo.allDay,
+      });
+    }
+  };
+  
+  const handleEventClick = (clickInfo) => {
+    
+      clickInfo.event.remove();
+    
+  };
+  
+  const handleEvents = (events) => {
+    setcurrentEvents(events);
+  };
+  
+  function renderEventContent(eventInfo) {
+    return (
+      <>
+        <b>{eventInfo.timeText}</b>
+        <i>{eventInfo.event.title}</i>
+      </>
+    );
+  }
+  function renderSidebarEvent(event) {
+    return (
+      <li key={event.id}>
+        <b>
+          {formatDate(event.start, {
+            year: "numeric",
+            month: "short",
+            day: "numeric",
+          })}
+        </b>
+        <i>{event.title}</i>
+      </li>
+    );
+  }
+  
+
   return (
     <div className="demo-app">
       {renderSidebar()}
@@ -42,88 +124,3 @@ export default function Calendar(props) {
   );
 }
 
-const renderSidebar = () => {
-  return (
-    <div className="demo-app-sidebar">
-      <div className="demo-app-sidebar-section">
-        <h2>Instructions</h2>
-        <ul>
-          <li>Select dates and you will be prompted to create a new event</li>
-          <li>Drag, drop, and resize events</li>
-          <li>Click an event to delete it</li>
-        </ul>
-      </div>
-      <div className="demo-app-sidebar-section">
-        <label>
-          <input
-            type="checkbox"
-            checked={weekendsVisible}
-            onChange={handleWeekendsToggle}
-          ></input>
-          toggle weekends
-        </label>
-      </div>
-      <div className="demo-app-sidebar-section">
-        <h2>All Events ({currentEvents.length})</h2>
-        <ul>{currentEvents.map(renderSidebarEvent)}</ul>
-      </div>
-    </div>
-  );
-};
-
-const handleWeekendsToggle = () => {
-  wseteekendsVisible(!weekendsVisible);
-};
-const handleDateSelect = (selectInfo) => {
-  let title = prompt("Please enter a new title for your event");
-  let calendarApi = selectInfo.view.calendar;
-
-  calendarApi.unselect(); // clear date selection
-
-  if (title) {
-    calendarApi.addEvent({
-      id: createEventId(),
-      title,
-      start: selectInfo.startStr,
-      end: selectInfo.endStr,
-      allDay: selectInfo.allDay,
-    });
-  }
-};
-
-const handleEventClick = (clickInfo) => {
-  if (
-    confirm(
-      `Are you sure you want to delete the event '${clickInfo.event.title}'`
-    )
-  ) {
-    clickInfo.event.remove();
-  }
-};
-
-const handleEvents = (events) => {
-  setcurrentEvents(events);
-};
-
-function renderEventContent(eventInfo) {
-  return (
-    <>
-      <b>{eventInfo.timeText}</b>
-      <i>{eventInfo.event.title}</i>
-    </>
-  );
-}
-function renderSidebarEvent(event) {
-  return (
-    <li key={event.id}>
-      <b>
-        {formatDate(event.start, {
-          year: "numeric",
-          month: "short",
-          day: "numeric",
-        })}
-      </b>
-      <i>{event.title}</i>
-    </li>
-  );
-}
