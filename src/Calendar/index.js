1b551cad747ba13c820c6522692d1d54eeefb55b
Muh-Hasan <muh.hasan.git@gmail.com>
2020-09-24 07:11:46 -0700

    changes

--- src/Calendar/index.js
+++ src/Calendar/index.js
@@ -4,6 +4,9 @@ import moment from "moment";
 export default function Calendar() {
   const [value, setValue] = useState(moment());
   const [calendar, setCalendar] = useState([]);
+  let startDay = value.clone().startOf("month").startOf("week");
+  let endDay = value.clone().endOf("month").endOf("week");
+  let day = startDay.clone().subtract(1, "day");
 
   useEffect(() => {
     let tempArray = [];
@@ -16,9 +19,6 @@ export default function Calendar() {
     }
     setCalendar(tempArray);
   }, [value]);
-  let startDay = value.clone().startOf("month").startOf("week");
-  let endDay = value.clone().endOf("month").endOf("week");
-  let day = startDay.clone().subtract(1, "day");
 
   return(
       <div className='calendar'>
