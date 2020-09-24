e6a75b9104c42ef450ca8e6bf9d587265d065db8
Muh-Hasan <muh.hasan.git@gmail.com>
2020-09-24 07:09:43 -0700

    updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import Calendar from './Calender/index'
+import Calendar from './Calendar/index'
 
 function App() {
   return (
---
+++ src/Calendar/index.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+import moment from "moment";
+
+export default function Calendar() {
+  const [value, setValue] = useState(moment());
+  const [calendar, setCalendar] = useState([]);
+
+  useEffect(() => {
+    let tempArray = [];
+    while (day.isBefore(endDay, "day")) {
+      tempArray.push(
+        Array(7)
+          .fill(0)
+          .map(() => day.add(1, "day").clone())
+      );
+    }
+    setCalendar(tempArray);
+  }, [value]);
+  let startDay = value.clone().startOf("month").startOf("week");
+  let endDay = value.clone().endOf("month").endOf("week");
+  let day = startDay.clone().subtract(1, "day");
+
+  return(
+      <div className='calendar'>
+          {calendar.map(week => <div>
+              {week.map(day => <div className='day'>
+                  {day.format('D')}
+              </div>)}
+          </div>)}
+      </div>
+  )
+}
