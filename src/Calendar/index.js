39d412a716faf744891c31080b8424348b6a18fc
Muh-Hasan <muh.hasan.git@gmail.com>
2020-09-24 07:30:32 -0700

    day styles done

--- src/Calendar/index.js
+++ src/Calendar/index.js
@@ -20,7 +20,21 @@ export default function Calendar() {
     setCalendar(tempArray);
   }, [value]);
 
-  
+  function isSelected(day) {
+    return value.isSame(day, "day");
+  }
+  function beforeToday(day) {
+    return day.isBefore(new Date(), "day");
+  }
+  function isToday(day) {
+    return day.isSame(new Date(), "day");
+  }
+  function dayStyles(day) {
+    if (beforeToday(day)) return "before";
+    if (isSelected(day)) return "selected";
+    if (isToday(day)) return "today";
+    return "";
+  }
 
   return (
     <div className="calendar">
@@ -28,7 +42,7 @@ export default function Calendar() {
         <div>
           {week.map((day) => (
             <div className="day" onClick={() => setValue(day)}>
-              <div className={value.isSame(day, "day") ? "selected" : ""}>
+              <div className={dayStyles(day)}>
                 {day.format("D").toString()}
               </div>
             </div>
